229ff620516b4ec5810428f225907b059049574c
ruhi <raktterruhi@gmail.com>
2022-05-25 05:18:39 +0600

    NotFound component added

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,11 +4,13 @@ const About = () => {
     return (
         <div>
             <h1 className='text-4xl mt-12 pb-3'>The Ever Inspiring Story of InstaX</h1>
-            <h2 className='text-xl font-mono mt-12 pb-12 px-7 text-left'>Whether it is cameras, printers or film, 
+            <h2 className='text-xl font-mono mt-12 pb-14 px-8 text-left'>Whether it is cameras, printers or film, 
                 Instax products inspire your customers to come together and share everything your 
                 business has to give. Especially the instax Link WIDE smartphone printer; its compact
                 and lightweight design makes it portable. And the fast, quality reprints add richness to any 
-                customer experience.</h2>
+                customer experience. The revival of vintage and a nostalgia for an analog past.
+                Instead of being phased out because it's old, it turned out to be the very reason for Instax's sudden resurgence. Nowadays, everything old is the new again, where old is the new new, and vintage is the new modern. <br />
+                To know more click the button below...</h2>
                 <button className=" ml-7 rounded-3xl 
                     my-9 py-2 w-3/12 bg-sky-900 text-white hover:bg-cyan-700">
                     <a href="https://fujixweekly.com/2022/01/18/fujis-instant-film-the-immensely-interesting-story-of-instax/" target="_blank" rel="noopener noreferrer">Check Out</a>
--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 const NotFound = () => {
     return (
-        <div>
-            <h2>404</h2>
-        </div>
+    <div>
+        <h2 className='text-4xl py-12 mb-14'> Page Not Found</h2>
+        <img className='w-3/12 mx-14 mb-14 ' src="https://i.ibb.co/BqZ6wVw/404.jpg" alt="" />
+    </div>
     );
 };
 
